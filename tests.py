723db7f515cb63da9e11580a9397c5177980cadd
Igor Teixeira <igor.teixeira@cromai.com>
2022-03-10 15:49:11 -0300

    closes #1; Use flask test client to create request contexts for tests

--- tests.py
+++ tests.py
@@ -1,11 +1,17 @@
-from main import hello_name, hello_world
+from main import app, hello_name, hello_world
 from unittest import TestCase
 
 
 class BasicTests(TestCase):
 
     def test_hello_world(self):
-        self.assertEqual(hello_world(),"<p>Hello, World!</p>")
+        app.testing = True
+        with app.test_client() as client:
+            response = client.get('/')
+        self.assertEqual(response.data, b"<p>Hello, World!</p>")
 
     def test_hello_name(self):
-        self.assertEqual(hello_name(), "<p>Hello, anonymous!</p>")
+        app.testing = True
+        with app.test_client() as client:
+            response = client.get('/name')
+        self.assertEqual(response.data, b"<p>Hello, anonymous!</p>")
