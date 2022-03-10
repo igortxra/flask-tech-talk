165d556abd32cf53adffd420e42b6bf85925b116
Igor Teixeira <igor.teixeira@cromai.com>
2022-03-10 15:10:22 -0300

    Add test for the new route
    I can not test my routes that way because many times they require a request context
    
    If you try to run tests now, a RuntimeError will be raised: `RuntimeError: Working outside of request context.`

--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-from main import hello_world
+from main import hello_name, hello_world
 from unittest import TestCase
 
 
@@ -6,3 +6,6 @@ class BasicTests(TestCase):
 
     def test_hello_world(self):
         self.assertEqual(hello_world(),"<p>Hello, World!</p>")
+
+    def test_hello_name(self):
+        self.assertEqual(hello_name(), "<p>Hello, anonymous!</p>")
