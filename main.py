3581afaa4f900afe199ff4d6ab27152bf7bf0511
Igor Teixeira <igor.teixeira@cromai.com>
2022-03-10 15:04:20 -0300

    Add /name route
    This route will use the request context to get query string parameters, so the test for this one will be different

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 
 app = Flask(__name__)
@@ -7,3 +7,9 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/name")
+def hello_name():
+    name = request.args.get("n", "anonymous")
+    return f"<p>Hello, {name}!</p>"
