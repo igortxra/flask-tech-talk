2e21231f277496f11bbc9711f22d5bb0ffead771
Igor Teixeira <igor.teixeira@cromai.com>
2022-03-14 11:22:29 -0300

    Use pytest for tests
    I think pytest makes everything easier, i can use pytest.fixtures now

--- test_main.py
+++ test_main.py
@@ -1,23 +1,23 @@
 from main import app
-from unittest import TestCase
+import pytest
 
 
-class BasicTests(TestCase):
+class TestBasics:
 
     def test_hello_world(self):
         app.testing = True
         with app.test_client() as client:
             response = client.get('/')
-        self.assertEqual(response.data, b"<p>Hello, World!</p>")
+        assert response.data == b"<p>Hello, World!</p>"
 
     def test_hello_name(self):
         app.testing = True
         with app.test_client() as client:
             response = client.get('/name')
-        self.assertEqual(response.data, b"<p>Hello, anonymous!</p>")
+        assert response.data == b"<p>Hello, anonymous!</p>"
 
     def test_hello_name_passing_name(self):
         app.testing = True
         with app.test_client() as client:
             response = client.get("/name?n=Jhon Doe")
-        self.assertEqual(response.data, b"<p>Hello, Jhon Doe!</p>")
+        assert response.data == b"<p>Hello, Jhon Doe!</p>"
