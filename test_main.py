eb56facfc10c0091f9b8dbda4b1d5969fa3c1ce0
Igor Teixeira <igor.teixeira@cromai.com>
2022-03-15 10:37:32 -0300

    Use pytest fixture to set test client for each test

--- test_main.py
+++ test_main.py
@@ -2,22 +2,23 @@ from main import app
 import pytest
 
 
-class TestBasics:
+@pytest.fixture
+def client():
+    app.testing = True # Change FLask internal behabior to make tests easier
+    client = app.test_client()
+    return client
 
-    def test_hello_world(self):
-        app.testing = True
-        with app.test_client() as client:
-            response = client.get('/')
-        assert response.data == b"<p>Hello, World!</p>"
 
-    def test_hello_name(self):
-        app.testing = True
-        with app.test_client() as client:
-            response = client.get('/name')
-        assert response.data == b"<p>Hello, anonymous!</p>"
+def test_hello_world(client):
+    response = client.get('/')
+    assert response.data == b"<p>Hello, World!</p>"
 
-    def test_hello_name_passing_name(self):
-        app.testing = True
-        with app.test_client() as client:
-            response = client.get("/name?n=Jhon Doe")
-        assert response.data == b"<p>Hello, Jhon Doe!</p>"
+
+def test_hello_name(client):
+    response = client.get('/name')
+    assert response.data == b"<p>Hello, anonymous!</p>"
+
+
+def test_hello_name_passing_name(client):
+    response = client.get("/name?n=Jhon Doe")
+    assert response.data == b"<p>Hello, Jhon Doe!</p>"
